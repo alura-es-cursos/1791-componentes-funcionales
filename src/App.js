37d6d903b86fe4a980cf9c59c455181dd3f9e24a
Harland Lohora <harland@lohora.com>
2021-12-08 19:12:09 -0300

    clase02 completada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Container from '@mui/material/Container'
 import FormSignUp from './components/FormSignUp';
+import Typography from '@mui/material/Typography'
 function App() {
   return (
-    <>
-      <h1>Formulario Registro</h1>
+    <Container component="section" maxWidth="sm">
+      <Typography variant="h3" align="center" component="h1">Formulario Registro</Typography>
       <FormSignUp />
-    </>
+    </Container>
   );
 }
 
--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,18 +1,49 @@
 import Button from "@mui/material/Button"
+import TextField from "@mui/material/TextField"
+import Switch from "@mui/material/Switch"
+import FormGroup from "@mui/material/FormGroup"
+import FormControlLabel from "@mui/material/FormControlLabel"
+import { FormControl } from "@mui/material"
 
 function FormSignUp() {
 	return (
 		<form>
-			<label>Nombre</label>
-			<input type="text" />
-			<label>Apellidos</label>
-			<input type="text" />
-			<label>Correo electronico</label>
-			<input type="email" />
-			<label>Promociones</label>
-			<input type="checkbox" />
-			<label>Novedades</label>
-			<input type="checkbox" />
+			<TextField
+				id="name"
+				label="Nombre"
+				variant="outlined"
+				fullWidth
+				margin="normal"
+			/>
+			<TextField
+				id="lastName"
+				label="Apellidos"
+				variant="outlined"
+				fullWidth
+				margin="normal"
+			/>
+			<TextField
+				id="email"
+				label="Email"
+				variant="outlined"
+				fullWidth
+				margin="normal"
+			/>
+			<FormGroup>
+				<FormControlLabel
+					control={
+						<Switch defaultChecked />
+					}
+					label="Promociones"
+				/>
+
+				<FormControlLabel
+					control={
+						<Switch defaultChecked />
+					}
+					label="Novedades"
+				/>
+			</FormGroup>
 			<Button variant="contained">
 				Registrarse
 			</Button>

5467e454c976252fb7ec84f387e3f18a1b1ee8ab
Harland Lohora <harland@lohora.com>
2021-12-14 20:33:51 -0300

    clase04 completada

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import Container from '@mui/material/Container'
 import FormSignUp from './components/FormSignUp';
 import Typography from '@mui/material/Typography'
 function App() {
+
+  const handleSubmit = (valores) => {
+    console.log('APPJS: ', valores)
+  }
+
   return (
     <Container component="section" maxWidth="sm">
       <Typography variant="h3" align="center" component="h1">Formulario Registro</Typography>
-      <FormSignUp />
+      <FormSignUp handleSubmit={handleSubmit} />
     </Container>
   );
 }
--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,7 +1,4 @@
-import {
-	useState,
-	useEffect,
-} from "react"
+import { useState } from "react"
 import Button from "@mui/material/Button"
 import TextField from "@mui/material/TextField"
 import Switch from "@mui/material/Switch"
@@ -9,23 +6,36 @@ import FormGroup from "@mui/material/FormGroup"
 import FormControlLabel from "@mui/material/FormControlLabel"
 import { FormControl } from "@mui/material"
 
-function FormSignUp() {
+function FormSignUp({ handleSubmit }) {
 	const [name, setName] = useState("")
-	useEffect(() => {
-		console.log("Name cambio: ", name)
-	}, [name])
+	const [lastName, setLastName] =
+		useState("")
+	const [email, setEmail] = useState("")
+	const [prom, setProm] = useState(true)
+	const [nov, setNov] = useState(false)
+
 	return (
-		<form>
+		<form
+			onSubmit={(e) => {
+				e.preventDefault()
+				handleSubmit({
+					name,
+					lastName,
+					email,
+					prom,
+					nov,
+				})
+			}}
+		>
 			<TextField
 				id="name"
 				label="Nombre"
 				variant="outlined"
 				fullWidth
 				margin="normal"
-				onChange={(e) => {
-					console.log(e.target.value)
+				onChange={(e) =>
 					setName(e.target.value)
-				}}
+				}
 				value={name}
 			/>
 			<TextField
@@ -34,6 +44,10 @@ function FormSignUp() {
 				variant="outlined"
 				fullWidth
 				margin="normal"
+				value={lastName}
+				onChange={(e) =>
+					setLastName(e.target.value)
+				}
 			/>
 			<TextField
 				id="email"
@@ -41,27 +55,51 @@ function FormSignUp() {
 				variant="outlined"
 				fullWidth
 				margin="normal"
+				value={email}
+				onChange={(e) =>
+					setEmail(e.target.value)
+				}
 			/>
 			<FormGroup>
 				<FormControlLabel
 					control={
-						<Switch defaultChecked />
+						<Switch
+							checked={prom}
+							onChange={(e) =>
+								setProm(
+									e.target.checked
+								)
+							}
+						/>
 					}
 					label="Promociones"
 				/>
 
 				<FormControlLabel
 					control={
-						<Switch defaultChecked />
+						<Switch
+							checked={nov}
+							onChange={(e) =>
+								setNov(e.target.checked)
+							}
+						/>
 					}
 					label="Novedades"
 				/>
 			</FormGroup>
-			<Button variant="contained">
+			<Button
+				variant="contained"
+				type="submit"
+			>
 				Registrarse
 			</Button>
 		</form>
 	)
+}
+
+function CrearHook() {
+	const [a, setA] = useState("a")
+	return <></>
 }
 
 export default FormSignUp
